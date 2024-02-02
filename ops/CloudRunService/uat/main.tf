735c17c8cd4e97c502ca24883c78210a4f938151
Haritha Chakali <HR319@sgpltech.com>
2024-02-02 23:08:28 +0530

    adding code

--- ops/CloudRunService/uat/main.tf
+++ ops/CloudRunService/uat/main.tf
@@ -1,5 +1,13 @@
 
 terraform {
+  required_version = ">= 1.7.1, < 1.8.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.69.1"
+    }
+  }
+}terraform {
   required_version = "~> 0.14"
   required_providers {
     google = {
